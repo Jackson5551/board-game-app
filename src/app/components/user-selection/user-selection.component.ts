711c52f87eb7332aaa094a72e48276809615625b
Jackson5551 <45270054+Jackson5551@users.noreply.github.com>
2023-04-19 10:11:38 -0600

    part-11

--- src/app/components/user-selection/user-selection.component.ts
+++ src/app/components/user-selection/user-selection.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ListType } from 'src/app/enums/list-types';
 import { Game } from 'src/app/interfaces/game';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import * as _ from 'lodash';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-user-selections',
@@ -10,11 +12,15 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
   styleUrls: ['./user-selection.component.scss']
 })
 export class UserSelectionsComponent implements OnInit{
+  unsubscribe = new Subject<void>();
 
   listType: ListType;
   listTypeTitle: string;
   ownedGames: Game[];
   wishListGames: Game[];
+
+  dataSource;
+  displayedColumns = ['name', 'image', 'min_age', 'min_players', 'max_players', 'min_playtime', 'delete'];
 
   constructor(
     private route: ActivatedRoute,
@@ -24,5 +30,38 @@ export class UserSelectionsComponent implements OnInit{
   ngOnInit(): void {
     this.listType = this.route.snapshot.routeConfig.path === 'owned' ? ListType.OWNEDLIST : ListType.WISHLIST;
     this.listTypeTitle = this.listType === ListType.OWNEDLIST ? 'Owned Games' : 'Wishlist Games';
+
+    if (this.listType === ListType.OWNEDLIST) {
+      this.localStorageService.getGameList(ListType.OWNEDLIST);
+      this.localStorageService.ownedGames.pipe(
+        takeUntil(this.unsubscribe),
+        tap(games => {
+          this.ownedGames = games;
+          this.dataSource = this.ownedGames;
+        })
+      ).subscribe();
+    } else {
+      this.localStorageService.getGameList(ListType.WISHLIST);
+      this.localStorageService.wishListGames.pipe(
+        takeUntil(this.unsubscribe),
+        tap(games => {
+          this.wishListGames = games;
+          this.dataSource = this.wishListGames;
+        })
+      ).subscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  deleteGame(game: Game) {
+    if (this.listType === ListType.OWNEDLIST) {
+      this.localStorageService.deleteGame(game, ListType.OWNEDLIST);
+    } else {
+      this.localStorageService.deleteGame(game, ListType.WISHLIST);
+    }
   }
 }
